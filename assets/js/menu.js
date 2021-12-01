70746f55f60892a0ae9846dc3fa9aeda089ec03c
tropiplaya <66205065+tropiplaya@users.noreply.github.com>
2021-12-01 01:39:31 +0100

    add categories

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -17,18 +17,23 @@ $(document).ready(function () {
     }).done(function (data) {
         readCSV(data);
     });
+
 });
+
 
 function readCSV(data) {
     let arr = $.csv.toArrays(data);
-    let dishes = {};
+    let dishes = [];
     arr.forEach(
         function (elem, index) {
             if (index < 1) return false;
-            //console.log(elem);
+            console.log(elem);
             if (elem[0] && elem[1] && elem[2] && elem[3]) {
+
                 let safeString = btoa(elem[0]);
                 if (!dishes.hasOwnProperty(safeString)) {
+
+                    console.log(safeString);
                     dishes[safeString] = [];
                 }
                 dishes[safeString].push(elem);
@@ -46,12 +51,13 @@ function buildMenu(dishes) {
     for (const keyCategory in dishes) {
         if (dishes.hasOwnProperty(keyCategory)) {
             let categoryList = dishes[keyCategory];
-            let rowID = "category" + keyCategory.slice(0, -2);
+            let rowID = "category" + keyCategory.replace(/=/g,'');
             console.info(rowID);
             $("#dish-category").append('<li class="nav-item"><a class="nav-link" href="#' + rowID + '">' + atob(keyCategory) + '</a></li>');
 
             if ($("#" + rowID).length == 0) {
                 $("#dish-list").append('<div class="row" id="' + rowID + '"></div>');
+                $("#" + rowID).append('<p class="text-light h3 text-center">' + atob(keyCategory) + '</p>');
             }
             console.log(categoryList);
             for (const keyDish in categoryList) {
@@ -60,11 +66,22 @@ function buildMenu(dishes) {
                 let dishCard = '<div id="' + dishCardID + '" class="col-xs-12 col-md-6 col-xl-4 p-5"></div>';
                 $("#" + rowID).append(dishCard);
 
-                $("#" + dishCardID).append('<img src="./assets/images/dishes/dish' + categoryList[keyDish][1] + '.JPG" class="img-fluid rounded p-4 d-block" onerror="this.onerror=null; this.src=\'./assets/images/tangchu-logo.png\'" >');
+                $("#" + dishCardID).append('<img src="./assets/images/tangchu/tangchu-' + categoryList[keyDish][1] + '.JPG" class="img-fluid rounded p-4 d-block" onerror="this.onerror=null; this.src=\'./assets/images/tangchu/tangchu-bar.JPG\'" >');
                 $("#" + dishCardID).append('<p class="h5 text-light text-center">' + categoryList[keyDish][2] + '</p>');
                 $("#" + dishCardID).append('<p class="h4 text-light text-center">' + categoryList[keyDish][3] + '</p>');
 
             }
         }
     }
+}
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
